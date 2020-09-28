220f4ef7eb1e365e5e55640b1fbbf8572dfa0ccc
Hideo Suzuki <hsuzuki@labellevie.jp>
2020-09-28 21:21:41 +0900

    feat: add openjdk
    using bfg

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ export EDITOR=nvim
 alias vi='nvim'
 alias vim='nvim'
 alias rm='rm -i'
+alias bfg='java -jar ~/.local/lib/bfg.jar'
 
 # fisherman
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
@@ -14,32 +15,31 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 
-
 # nodebrew
-set -x PATH $HOME/.nodebrew/current/bin $PATH 
-
-# VSCode
-set -x PATH $PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+# set -x PATH $HOME/.nodebrew/current/bin $PATH 
 
 # added by pipsi (https://github.com/mitsuhiko/pipsi)
-set -x PATH /Users/hideoSuzuki/.local/bin $PATH
+# set -x PATH /Users/hideoSuzuki/.local/bin $PATH
 
 # pyenv
-status --is-interactive; and source (pyenv init -|psub)
+# status --is-interactive; and source (pyenv init -|psub)
 
 # OpenSSL
-set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+# set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 # For compilers to find openssl@1.1 you may need to set:
-set -gx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
+# set -gx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib"
+# set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
+set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 
 # For pkg-config to find openssl@1.1 you may need to set:
-set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
+# set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
 
 # Poetry
-set -g fish_user_paths "$HOME/.poetry/bin" $fish_user_paths
+# set -g fish_user_paths "$HOME/.poetry/bin" $fish_user_paths
+
+#
+set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
+
+# openjdk
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+set -gx CPPFLAGS "-I/usr/local/opt/openjdk/include"
